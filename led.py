4303da97521714c2c29240101240d1048891243b
Ted Pinkerton <ted.pinkerton@mail.utoronto.ca>
2022-12-05 02:52:07 -0500

    Added asynchronous polling

--- led.py
+++ led.py
@@ -2,10 +2,12 @@ import os
 import json
 import time
 import requests
+import threading
 
 from datetime import datetime
 from typing import List
 
+import numpy as np
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
@@ -62,13 +64,16 @@ class Event:
         self.cache_data = cache_data
         self.logging_file_name = logging_file_name
 
+        self.plate_tracker = threading.Thread(target=self.plate_reader, args=(1000,))
+        self.plate_tracker.start()
+
     def main_loop(self) -> None:
         """Main loop to run the event. Send the playlist in plate_files[# of plates] whenever a plate is changed."""
         if self.cache_data:
             # Read all the json files
             plate_data = tuple(read_json(file_name) for file_name in self.plate_files)
         # Intalize the plate pins and old_plates out of range
-        plates, old_plates = [plate.state() for plate in self.plates], []
+        plates, old_plates = self.plate_states, []
         # Run forever
         while True:
             # Check if the number of plates activaited has changed
@@ -88,8 +93,30 @@ class Event:
                 old_plates = plates
                 # Wait to avoid flickering if broken
                 time.sleep(1)
-            # Find the current state
-            plates = [plate.state() for plate in self.plates]
+            # Find the current state of the event
+            plates = self.plate_states
+
+    def plate_reader(self, max_poll_history: int = 1000):
+        """Asynchronusly keep track of the plate history.
+        
+        Args:
+            max_poll_history (int): Number of previous polls to consider (default: `1000`)
+        """
+        lock = threading.Lock()
+        plates_history = np.empty((len(self.plates),))
+        while True:
+            with lock:
+                # Read the current states
+                plate_history.append(self.read_plate_states())
+                # Restrict the number of polls considered
+                if len(plate_history) > max_poll_history:
+                    plate_history = numpy.delete(plate_history, (0), axis=0)
+                # Average and cast to int
+                self.plate_states = np.mean(plate_history, axis=0, dtype=np.int32)
+
+    def read_plate_states(self):
+        """Return the state of all the plates"""
+        return [plate.state() for plate in self.plates]
 
     def __del__(self):
         # Finish with the gpio pins
